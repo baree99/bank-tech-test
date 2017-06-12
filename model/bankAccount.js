cd083ec8f6b2c4eb874ad2763d46d67b67981889
Peter Barcsak <peter.barcsak@gmail.com>
2017-06-12 12:24:40 +0100

    When someone deposit it adds the amount to the account balance

--- model/bankAccount.js
+++ model/bankAccount.js
@@ -2,4 +2,8 @@ const BankAccount = function() {
     this.balance = 0;
 }
 
+BankAccount.prototype.deposit = function (amount) {
+  this.balance += amount;
+};
+
 module.exports = BankAccount;
--- test/bankAccountSpec.js
+++ test/bankAccountSpec.js
@@ -1,10 +1,22 @@
 var assert = require('assert');
 var expect = require('chai').expect;
-const BankAccount = require("../model/bankAccount.js")
+const BankAccount = require('../model/bankAccount.js')
 
-describe("BankAccount", function() {
-  it("initialize with zero balance", function() {
-    var bankAccount = new BankAccount();
+var bankAccount;
+
+beforeEach(function() {
+  bankAccount = new BankAccount();
+});
+
+describe('BankAccount', function() {
+  it('initialize with zero balance', function() {
     expect(bankAccount.balance).to.equal(0)
+  });
+
+  describe('deposit', function() {
+    it('adds the amount to the account balance', function() {
+      bankAccount.deposit(1000)
+      expect(bankAccount.balance).to.equal(1000)
+    });
   });
 });
